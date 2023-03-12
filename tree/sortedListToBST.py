83cee7f67c76f9e573ce3650f07645b968c8a770
Ippili Akarsh <65852816+akarshippili@users.noreply.github.com>
2023-03-12 08:48:44 +0000

    updated sortedListToBST.py

--- tree/sortedListToBST.py
+++ tree/sortedListToBST.py
@@ -1,14 +1,14 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 
@@ -25,7 +25,7 @@ class Solution:
         return prev
 
 
-    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+    def sortedListToBST(self, head):
         if(not head):return 
         if(not head.next): return TreeNode(head.val)  
         
@@ -40,3 +40,20 @@ class Solution:
         root.right = self.sortedListToBST(nextnode)
 
         return root
+    
+    def inorder(self, node):
+        if(not node): return
+        self.inorder(node.left)
+        print(node.val)
+        self.inorder(node.right)
+
+node = ListNode(-10)
+node.next  = ListNode(-2)
+node.next.next  = ListNode(3)
+node.next.next.next  = ListNode(5)
+node.next.next.next.next  = ListNode(10)
+node.next.next.next.next.next  = ListNode(12)
+
+Solution = Solution()
+root = Solution.sortedListToBST(node)
+Solution.inorder(root)
