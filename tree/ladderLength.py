20023e6d09a75fe66371912c33d8e992d0d2f2e6
Ippili Akarsh <65852816+akarshippili@users.noreply.github.com>
2023-06-09 01:57:53 +0530

    Create ladderLength.py

---
+++ tree/ladderLength.py
@@ -0,0 +1,57 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
+        words = {}
+        for index, word in enumerate(wordList): words[word] = index
+        if(endWord not in words): return 0
+
+        src = -1
+        dest = words[endWord]
+
+        if(beginWord not in words): 
+            wordList.append(beginWord)
+            src = len(wordList)-1
+        else:
+            src = wordList.index(beginWord)
+        
+        N = len(wordList)
+        graph = {}
+        for index in range(N): graph[index] = set()
+        
+        for wordIndex in range(N):
+            word = wordList[wordIndex]
+            for index in range(len(word)):
+                for ch in 'abcdefghijklmnopqrstuvwyz':
+                    new = word[:index] + ch + word[index+1:]
+                    if(new not in words): continue 
+                    node = words[new]
+                    graph[wordIndex].add(node)
+                    graph[node].add(wordIndex)
+
+        def bfs(root, dest):
+            queue = [root]
+            visited = set()
+            visited.add(root)
+            l = 1
+            depth = 1
+
+            while(queue):
+                cur = queue.pop(0)
+                l -= 1
+                if(cur == dest): return depth 
+
+                for neigh in graph[cur]:
+                    if(neigh in visited): continue
+                    if(neigh == dest): return depth+1
+                    visited.add(neigh)
+                    queue.append(neigh)
+                
+                if(l == 0):
+                    l = len(queue)
+                    depth += 1
+            
+            return -1
+
+        depth = bfs(src, dest)
+        if(depth == -1): return 0
+        return depth
