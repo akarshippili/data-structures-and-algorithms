be7b2317261b6a55e71871771f5351f0e7e32880
Ippili Akarsh <65852816+akarshippili@users.noreply.github.com>
2024-02-19 01:49:58 +0530

    Update mostBooked.py
    upgrade

--- heap/mostBooked.py
+++ heap/mostBooked.py
@@ -43,3 +43,50 @@ class Solution:
 
         # print(counter)
         return bestIndex
+
+
+
+class SolutionV2:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        
+        N = len(meetings)
+        meetings.sort()
+        counter = [0 for _ in range(n)]
+        heap = []
+        
+        # no need for sorted set we can do with heap
+        s = [index for index in range(n)]
+        heapify(s)
+
+
+        index = 0
+        while(index < N):
+
+            start, _ = meetings[index]
+            while(heap and heap[0][0] <= start): 
+                _, room = heappop(heap)
+                heappush(s, room)
+
+
+            if(len(heap) < n):
+                counter[s[0]] += 1
+                val = heappop(s) 
+                heappush(heap, [meetings[index][-1], val])
+                index += 1
+                continue
+            
+            end, room = heappop(heap)
+            counter[room] += 1
+            diff = meetings[index][-1] - meetings[index][0]
+            heappush(heap, [end + diff, room])
+            index += 1
+        
+
+        best = counter[0]
+        bestIndex = 0
+        for index in range(n):
+            if(counter[index] > best):
+                best = counter[index]
+                bestIndex = index
+
+        return bestIndex
