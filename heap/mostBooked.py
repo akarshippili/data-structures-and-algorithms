0dd5280d8b37ef5d1690908ad4f76bd20ce3a0c4
Ippili Akarsh <65852816+akarshippili@users.noreply.github.com>
2024-02-19 01:47:08 +0530

    Create mostBooked.py

---
+++ heap/mostBooked.py
@@ -0,0 +1,45 @@
+from sortedcontainers import SortedSet
+
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        
+        N = len(meetings)
+        meetings.sort()
+        counter = [0 for _ in range(n)]
+        s = SortedSet([index for index in range(n)])
+        # print(s[0])
+        heap = []
+
+
+        index = 0
+        while(index < N):
+
+            start, _ = meetings[index]
+            while(heap and heap[0][0] <= start): 
+                _, room = heappop(heap)
+                s.add(room)
+
+
+            if(len(heap) < n):
+                counter[s[0]] += 1
+                val = s.pop(index = 0) 
+                heappush(heap, [meetings[index][-1], val])
+                index += 1
+                continue
+            
+            end, room = heappop(heap)
+            counter[room] += 1
+            diff = meetings[index][-1] - meetings[index][0]
+            heappush(heap, [end + diff, room])
+            index += 1
+        
+
+        best = counter[0]
+        bestIndex = 0
+        for index in range(n):
+            if(counter[index] > best):
+                best = counter[index]
+                bestIndex = index
+
+        # print(counter)
+        return bestIndex
