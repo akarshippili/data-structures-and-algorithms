b3e7c72a3812ac5d1b0bdf2e4eee88c51609bbbd
Ippili Akarsh <65852816+akarshippili@users.noreply.github.com>
2024-04-01 03:55:08 +0530

    Update countSubarraysWithFixedBounds.py

--- arrays/two-pointers/countSubarraysWithFixedBounds.py
+++ arrays/two-pointers/countSubarraysWithFixedBounds.py
@@ -38,7 +38,67 @@ class Solution:
         
         return ans
 
-            
+class Solution2:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        
+        N = len(nums)
+        start_index = end_index = 0
+        queue = [[], []]
+        ans = 0
+        next_ = 0
 
+        while(end_index < N):
+            if(minK <= nums[end_index] <= maxK): 
+                if(nums[end_index] == minK or nums[end_index] == maxK):
+                    if(nums[end_index] == minK): queue[0].append(end_index)
+                    if(nums[end_index] == maxK): queue[-1].append(end_index)
+                end_index += 1
+            else:
+                """
+                    [start_index, end_index)
+                """
+                # print(start_index, end_index, queue)
+                while(queue[0] and queue[1]):
+                    if(queue[0][0] < queue[-1][0]):
+                        start = next_
+                        while(queue[0] and queue[0][0] < queue[-1][0]): end = queue[0].pop(0)
+                        ans += (end_index - queue[-1][0]) * (end - next_ + 1)
+                        next_ = end + 1
 
-            
+                    elif(queue[0][0] > queue[-1][0]):
+                        start = next_
+                        while(queue[-1] and queue[0][0] > queue[-1][0]): end = queue[-1].pop(0)
+                        ans += (end_index - queue[0][0]) * (end - next_ + 1)
+                        next_ = end + 1
+
+                    else:
+                        ans += end_index - queue[0][0]
+                        queue[-1].pop(0)
+                        queue[0].pop(0)
+                
+               
+                end_index += 1
+                start_index = end_index
+                next_ = start_index 
+                queue = [[], []]
+        
+        
+        while(queue[0] and queue[1]):
+            if(queue[0][0] < queue[-1][0]):
+                start = next_
+                while(queue[0] and queue[0][0] < queue[-1][0]): end = queue[0].pop(0)
+                ans += (end_index - queue[-1][0]) * (end - next_ + 1)
+                next_ = end + 1
+
+            elif(queue[0][0] > queue[-1][0]):
+                start = next_
+                while(queue[-1] and queue[0][0] > queue[-1][0]): end = queue[-1].pop(0)
+                ans += (end_index - queue[0][0]) * (end - next_ + 1)
+                next_ = end + 1
+
+            else:
+                ans += end_index - queue[0][0]
+                queue[-1].pop(0)
+                queue[0].pop(0)
+        
+        return ans
